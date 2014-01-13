bb48c73c1e562191e1c93f2660407e9c888e54aa
kevincr <crowder.kevin@gmail.com>
2014-01-12 22:29:52 -0800

    Put missing code back

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,35 @@
 class MembersController < ApplicationController
+
+  def new
+
+  end
+  def create
+    #render text: params[:member].inspect
+    @member = Member.new(member_params)
+
+    @member.save
+    redirect_to @member
+  end
+
+  def destroy
+
+  end
+
+  def index
+    @members = Member.all
+  end
+
+  def edit
+
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
+  private
+  def member_params
+    params.require(:member).permit(:name,:active,:is_member,:join_date)
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140113053217) do
+ActiveRecord::Schema.define(version: 20140113061545) do
 
   create_table "addresses", force: true do |t|
     t.string   "address_type"
